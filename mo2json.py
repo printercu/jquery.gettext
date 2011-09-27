599f38372adaba1ecdcab82a45b0956ff8e3d613
Eunchong Yu <kroisse@gmail.com>
2011-09-27 17:01:22 -0400

    Eliminated all of tabs in `mo2json.py`, and polished some codes.

--- mo2json.py
+++ mo2json.py
@@ -1,12 +1,12 @@
 try:
-	import json as json
+    import json
 except:
-	import simplejson as json
-
+    import simplejson as json
 import gettext
 import sys
 
-def gettext_json(domain, path, lang = [], indent = False):
+
+def gettext_json(domain, path, lang=[], indent=False):
     tr = gettext.translation(domain, path, lang)
     # for unknown reasons, instead of having plural entries like
     # key: [sg, pl1...]
@@ -22,17 +22,16 @@ def gettext_json(domain, path, lang = [], indent = False):
             ret[k[0]].append(v)
         else:
             ret[k] = v
-    return json.dumps(ret, ensure_ascii = False, indent = indent)
-    
+    return json.dumps(ret, ensure_ascii=False, indent=indent)
+
+
 def main():
-	domain = sys.argv[1]
-	path = sys.argv[2]
-	locale = sys.argv[3]
-	filename = sys.argv[4]
-	result = gettext_json(domain, path, [locale])
-	fileout = open(filename, "w")
-	fileout.write(result)
-	fileout.close()
+    domain, path, locale, filename = sys.argv
+    result = gettext_json(domain, path, [locale])
+    fileout = open(filename, "w")
+    fileout.write(result)
+    fileout.close()
+
 
 if __name__ == "__main__":
     main()
